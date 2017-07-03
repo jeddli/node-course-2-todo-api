931a10ee346763113fc678eca2227a02cb29062a
Lee dong hyeun <jeddli@gmail.com>
2017-07-03 20:22:15 +0900

    Add delete /todos/:id

--- server/server.js
+++ server/server.js
@@ -44,13 +44,30 @@ app.get('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
         res.send({todo});
-    }, (e) => {
+    } ).catch( (e) => {
         return res.status(400).send();
     } );
 
 });
 
+app.delete('/todos/:id', (req, res) => {
+    //get the id
+    var id = req.params.id;
+    // validate the id -> not valid? return 404
+    if( !ObjectID.isValid(id) ){
+        return res.status(404).send();
+    }
 
+    //remove todo by id
+    Todo.findByIdAndRemove(id).then( (todo) =>{
+        if(!todo){
+            return res.status( 404 ).send();
+        }
+        res.send(todo);
+    } ).catch( (e) => {
+        return res.status(400).send();
+    } );        
+} );
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
