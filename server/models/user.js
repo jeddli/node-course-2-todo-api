7c7a2d788d287c68fe1d11786433aa8cdf74b450
Lee dong hyeun <jeddli@gmail.com>
2017-07-04 17:26:28 +0900

    Modify user model and add user scheme

--- server/models/user.js
+++ server/models/user.js
@@ -1,26 +1,61 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('Users', {
-    name:{
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
+var UserSchema = new mongoose.Schema({
     email:{
         type: String,
-        required: true,
+        required: true,        
+        trim: true,
         minlength: 1,
-        trim: true
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: `{VALUE} is not a valid email`
+        }
     },
-    completed:{
-        type: Boolean,
-        default: false
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
     },
-    completedAt:{
-        type: Number,
-        default: null
-    }
+    tokens:[{
+        access:{
+            type: String,
+            required: true
+        },
+        token:{
+            type: String,
+            required: true
+        }
+    }]
 });
+
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+    console.log( 'User object', userObject );
+    var test =  _.pick(userObject, ['_id', 'email']);
+    
+    return test;
+};
+
+// can't use arrow functio, because you should use this..
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign( {_id: user._id.toHexString(), access}, 'abc123' ).toString();
+
+    user.tokens.push( {access, token});
+
+    // return other promise
+    return user.save().then( () => {        
+        return token;
+    });    
+};
+
+var User = mongoose.model('Users', UserSchema );
 
 module.exports = {User}
